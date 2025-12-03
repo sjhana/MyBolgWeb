b0b46a46686d919552ae9df2a6f35d9530944306
Aris <2765005260@qq.com>
2025-12-03 20:37:11 +0800

    修复了类型报错

--- compents/R3F/Models/rCompents/RotatingGroup.tsx
+++ compents/R3F/Models/rCompents/RotatingGroup.tsx
@@ -13,9 +13,9 @@ export default function RotatingGroup({speed = [0, 0, 0], children, rotation = [
     useFrame((state, delta) => {
     // delta 是两帧之间的时间间隔，乘以它可以保证旋转速度在不同刷新率屏幕上一致
     // speed 是旋转速度，数字越大转越快
-    groupRef.current.rotation.x += delta * speed[0]; 
-    groupRef.current.rotation.y += delta * speed[1]; 
-    groupRef.current.rotation.z += delta * speed[2]; 
+    groupRef.current!.rotation.x += delta * speed[0]; 
+    groupRef.current!.rotation.y += delta * speed[1]; 
+    groupRef.current!.rotation.z += delta * speed[2]; 
   });
   return (
   <group rotation={rotation} >
